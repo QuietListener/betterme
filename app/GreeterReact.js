bb3518fe1109a48a0fa091fcc14a3ba4caefa13e
junjun <junjun@baicizhan.com>
2018-01-01 16:14:32 +0800

    主页逻辑ok了

---
+++ app/GreeterReact.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import config from './conf/config.json';
+
+import "./css/Greeter.css"
+import styles from './css/Greeter.css';
+import RNWebviewMessager from "./lib/rn_webview_messager.js"
+
+console.log(styles)
+
+class GreeterReact extends Component{
+
+    constructor(props)
+    {
+      super(props);
+      this.state = {share_result:"no result",chanel:null};
+      this.msger = new RNWebviewMessager();
+      this.show = this.show.bind(this);
+      this.share_callback = this.share_callback.bind(this);
+    }
+
+
+    share_callback(result,chanel)
+    {
+      //alert(`chanel:${chanel}, result:${JSON.stringify(result)}`)
+      this.setState({result:result,chanel:chanel});
+
+    }
+
+    show()
+    {
+      alert("show--")
+      var url = "https://mango.baicizhan.com/react_reading/reading/daka_package?di=TPh7RzkGNfiIB8PU/KlTR7owIk2T0c8u78JsdoJmBSM="
+      var img = "https://7n.bczcdn.com/cover_img/rimg_package_999999999_1503051309_13505.jpeg"
+
+      var share_data = {"url": url, "title": "测试", "desc": "desc", "img": img, "share_type": "", "share_media": ""}
+      this.msger.share(onShareClick, this.share_callback)
+    }
+
+    render(){
+        return (
+            <div className={styles.root}>
+
+               <div> {config.greetText}</div>
+                <div>
+                  chanel:{this.state.chanel}
+                  <br/>
+                  result:{this.state.result==null?"没有":JSON.stringify(this.state.result)}
+                </div>
+
+                <div style={{fontSize:40}} onClick={this.show.bind(this)}>show</div>
+            </div>
+        )
+    }
+}
+
+export default GreeterReact
---
+++ app/lib/rn_webview_messager.js
@@ -0,0 +1,50 @@
+
+
+export default class RNWebviewMessager
+{
+  constructor()
+  {
+    this.call_backs={};
+    this.id = 0;
+
+    var that = this;
+    //alert("init...");
+    window.document.addEventListener('message', function (e) {
+      const message = e.data;
+
+      console.log(`from RN:${message}`);
+  
+      var ret = JSON.parse(message);
+      var {id, method, result} = JSON.parse(message);
+      var call_back = that.call_backs[id.toString() ];
+
+      //result必须为数组
+      if(call_back != null)
+        call_back(...result);
+    })
+  }
+
+
+   // share_data = {"url": url, "title": title, "desc": desc, "img": img, "share_type": share_type||"", "share_media": share_media||""}
+  share(share_data,call_back) {
+    var cmd = "share";
+    var share_data_ = {web_url:share_data.url, img_url:share_data.img, title:share_data.title, desc:share_data.desc};
+    this.call(cmd, share_data_ , call_back);
+   }
+
+
+  call(method, args, call_back)
+  {
+      var command = {method, args}
+      this.id = this.id+=1;
+      var id = this.id;
+      var command_ = {id,command}
+
+      var msg = JSON.stringify(command_);
+      console.log("to RN:"+msg);
+      this.call_backs[id.toString()]=call_back;
+
+      window.postMessage(msg);
+  }
+}
+

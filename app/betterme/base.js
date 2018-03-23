8977d76e9fc182a3dfb3d57ec2d090465d7d42a1
junjun <junjun@baicizhan.com>
2018-03-23 17:06:11 +0800

    fx

--- app/betterme/base.js
+++ app/betterme/base.js
@@ -3,8 +3,8 @@ import moment from "moment"
 
 export const DayMinSeconds = 24*60*60*1000;
 export const slogon = "做更好的自己"
-export const BaseHost = "http://localhost:3000"
-//export const BaseHost = `http://www.coderlong.com`
+//export const BaseHost = "http://localhost:3000"
+export const BaseHost = `http://www.coderlong.com`
 export const IMG_BASE = `${BaseHost}/upload/`
 
 const history = hashHistory;

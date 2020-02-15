0a9d7a2ee3288836408232b97248a303a262fffb
junjun <junjun@baicizhan.com>
2020-02-15 19:02:05 +0800

    fx

--- app/betterme/comment.js
+++ app/betterme/comment.js
@@ -23,31 +23,127 @@ export default class Comment extends Component
 
     this.state = {
       id: id,
+      commentContent:"",
+      data:{},
+      userMap:{}
     };
 
-    //this.load = this.load.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.submitComment = this.submitComment.bind(this);
+    this.load = this.load.bind(this);
+    this.commentDiv = this.commentDiv.bind(this);
   }
 
   componentDidMount()
   {
-    //this.load();
+    this.load();
   }
 
 
+  handleChange(propName, event)
+  {
+    let data = {}
+    data[propName] = event.target.value;
+    this.setState(data);
+  }
 
+  submitComment(){
+    var params = {
+      target_id:this.state.id,
+      content: this.state.commentContent
+    }
+
+    var url = `${BaseHost}/comment/addComment.json`;
+    console.log(url);
+    axios.post(url, params).then((res) => {
+      console.log("res", res);
+      this.load();
+    }).catch(e => {
+      console.log(e);
+      this.setState({loading: false});
+      this.load();
+    })
+  }
+
+
+  load()
+  {
+    var that = this;
+    this.setState({loading: true});
+
+    var id = this.state.id;
+    axios.get(`${BaseHost}/comment/getComments.json?target_id=${this.state.id}`).then((res) => {
+      console.log("res", res);
+      let data = res.data.data;
+
+      let users = data.users;
+      let userMap = this.state.userMap;
+      for(let u in users){
+        userMap[u] = users[u];
+      }
+      that.setState({data: data,userMap:userMap});
+
+      console.log(that.state);
+      // that.load_plans(user.id)
+    }).catch(e => {
+      console.log(e);
+      this.setState({loading: false});
+    })
+  }
+
+  commentDiv(c){
+    let userMap = this.state.userMap;
+    let user = userMap[c.user_id] || {}
+
+    let time = c.created_at.split("T")[0];
+    return <div style={{width:"100%",marginLeft:"8px",margin:"4px",marginBottom:"20px",fontSize:"14px"}}>
+      <div>
+      {user["name"]||"user"} {time}
+      </div>
+
+      <div style={{marginBottom:"8px",borderBottom:"1px solid green",fontSize:"14px"}}>
+        {c.content}
+      </div>
+
+    </div>
+  }
   render()
   {
+
+
+    var my_comments = this.state.data.my_comments||[];
+    var comments = this.state.data.comments||[];
+
+    var my_comments_divs = my_comments.map(c=>{
+        return this.commentDiv(c);
+    })
 
     return (
 
       <div>
-         comment
+        {/*评论框*/}
+        <div>
+         <div style={inner_style.textBox} onChange={event=>this.handleChange("commentContent",event)}>
+           <textarea  style={{width:"100%",height:"100px"}} value={this.state.commentContent} ></textarea>
+         </div>
+         <div style={{textAlign:"right"}}>
+              <div style={{margin:"4px"}} onClick={this.submitComment}> submit</div>
+         </div>
+
+        </div>
+
+        {/*展示所有评论*/}
+        <div>
+          {my_comments_divs}
+
+        </div>
+
       </div>
     );
   }
 }
 
 const inner_style = {
-  part: {display: "inline-block", verticalAlign: "top", width: "44%", fontSize: "10px"},
+  textBox: {display: "inline-block", verticalAlign: "top", width: "100%", fontSize: "10px"},
   input: {fontSize: "22px", minWidth: "120px", border: "0px", borderBottom: "1px solid #f2f2f2", marginTop: "10px"}
 }
